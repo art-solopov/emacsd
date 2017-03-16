1a8c3e2ac4a7ee676318ed25aa8c7cf057964aab
Artemiy Solopov <art-solopov@yandex.ru>
2017-03-17 01:02:54 +0300

    Pyenv

--- config/70-python.el
+++ config/70-python.el
@@ -1,5 +1,22 @@
 ;;; 90-python.el --- Various configurations for Python
 (require 'f)
+(require 's)
+
+;;; Pyenv
+
+(defun set-pyenv-project-version (project-path)
+  (let (
+	(python-version-path (f-join project-path ".python-version"))
+	)
+    (when (f-exists? python-version-path)
+      (let (
+	    (python-version (s-trim (f-read-text python-version-path)))
+	    )
+	(pyenv-mode-set python-version)
+	)
+      )
+   )
+    )
 
 ;;; Flycheck & virtualenv
 
@@ -21,6 +38,7 @@
     (let (
 	  (venv-path (f-join (projectile-project-root) "venv"))
 	  )
+      (set-pyenv-project-version (projectile-project-root))
       (pythonic-activate venv-path)
       (set-flymake-python-paths)
       )
